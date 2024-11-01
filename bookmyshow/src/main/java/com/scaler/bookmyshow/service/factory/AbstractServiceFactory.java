315999bbc4dd314752f16a73c1b00d1533fcfec8
Rajendra Kumar Mahapatra <rmahapatra1999@gmail.com>
2024-11-01 21:37:23 +0530

    AbstractService factory design implemented

--- bookmyshow/src/main/java/com/scaler/bookmyshow/service/factory/AbstractServiceFactory.java
+++ bookmyshow/src/main/java/com/scaler/bookmyshow/service/factory/AbstractServiceFactory.java
@@ -1,12 +1,39 @@
 package com.scaler.bookmyshow.service.factory;
 
-import java.util.List;
-import java.util.Map;
+import com.scaler.bookmyshow.advice.exception.ProgramException;
+import com.scaler.bookmyshow.service.annotation.BmsService;
+import com.scaler.bookmyshow.utils.BMSConstant;
+import org.springframework.core.annotation.AnnotationUtils;
+
+import java.util.*;
 
 public abstract class AbstractServiceFactory<T> {
-    public Map<String, T> serviceMap;
-    public T defaultService;
-    public AbstractServiceFactory(List<T> allService) {
 
+    private Map<String, T> serviceMap;
+    private T defaultService;
+
+    protected AbstractServiceFactory(List<T> services) {
+        Map<String, T> bmsServiceMap = new HashMap<>();
+        for (T service : services) {
+            BmsService bmsService = AnnotationUtils.getAnnotation(service.getClass(), BmsService.class);
+            assert bmsService != null;
+            if (bmsService.value().equals(BMSConstant.DEFAULT)) {
+                if (Objects.isNull(defaultService)) {
+                    defaultService = service;
+                    continue;
+                } else {
+                    throw new ProgramException("There can be only one default service of the Interface" + service.getClass());
+                }
+            }
+            bmsServiceMap.computeIfPresent(bmsService.value(), (carType, existedService) -> {
+                throw new ProgramException("There cant be duplicate service implementation of one interface." + existedService.getClass());
+            });
+            bmsServiceMap.put(bmsService.value(), service);
+            serviceMap = Collections.unmodifiableMap(bmsServiceMap);
+        }
+    }
+
+    public T getService(String type) {
+        return serviceMap.getOrDefault(type, defaultService);
     }
 }
